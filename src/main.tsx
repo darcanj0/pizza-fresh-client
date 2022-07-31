921ebeafc342a5df7fd5c1fd6a7e97c3fbe5789d
Daniel Arcanjo <daniel.thomas.aarcanjo@gmail.com>
2022-07-31 15:36:41 -0300

    configure theme provider

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+import { ThemeProvider } from "styled-components";
 import App from "./App";
+import theme from "./assets/styles/theme";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <ThemeProvider theme={theme}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </ThemeProvider>
   </React.StrictMode>
 );
